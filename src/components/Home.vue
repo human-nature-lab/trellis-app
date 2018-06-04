ee2d35f47bfbf4571b8c27e49455adfb038f390c
Mark McKnight <mark.mcknight@yale.edu>
2018-06-04 15:43:18 -0400

    Resolved some warnings, moved global isCordova from methods to computed.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,16 +9,11 @@
   import SyncService from './sync/services/SyncService'
   import Sync from './sync/Sync.vue'
   import Login from './login/Login.vue'
-  import config from '../config'
-  import constants from '../constants'
   export default {
     name: 'home',
     computed: {
       hasSynced: function () {
         return SyncService.hasSynced()
-      },
-      isCordova: function () {
-        return (config.appEnv === constants.APP_ENV.CORDOVA)
       }
     },
     components: {
