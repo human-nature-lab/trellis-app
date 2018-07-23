b08757b268b441710fe8803ebb290d017a097a8d
wyattis <wyattisrael@gmail.com>
2018-07-23 16:32:27 -0400

    Added an action mixin to simplify creating actions a little bit. Added question.types and action.types constants to help avoid simple spelling errors. Updated Question component to take advantage of 'is' directive. Added RespondentGeoQuestion component which uses the RespondentGeos component to manage respondent geos.

---
+++ src/components/interview/questions/RespondentGeoQuestion.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-flex class="respondent-geo-question">
+    <RespondentGeos
+      :respondent="respondent"
+      @after-add="afterAdd"
+      @after-move="afterMove"
+      @after-remove="afterRemove" />
+  </v-flex>
+</template>
+
+<script>
+  import RespondentGeos from '../../respondent/RespondentGeos'
+  import AT from '../../../static/action.types'
+  import ActionMixin from '../../../mixins/ActionMixin'
+  export default {
+    name: 'respondent-geo-question',
+    props: {
+      question: {
+        type: Object,
+        required: true
+      },
+      respondent: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+      afterMove (resGeo) {
+        this.action(AT.respondent_move, {
+          respondent_geo_id: resGeo.pivot.id,
+          val: 'Move respondent'
+        })
+      },
+      afterAdd (resGeo) {
+        this.action(AT.respondent_add_geo, {
+          respondent_geo_id: resGeo.pivot.id,
+          val: 'Add respondent geo'
+        })
+      },
+      afterRemove (resGeo) {
+        this.action(AT.respondent_remove_geo, {
+          respondent_geo_id: resGeo.pivot.id,
+          val: 'Remove respondent geo'
+        })
+      }
+    },
+    mixins: [ActionMixin],
+    components: {RespondentGeos}
+  }
+</script>
