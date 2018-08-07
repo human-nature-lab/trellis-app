4ec21262ad8cb34b2ca8f880dc78825489a1342e
Mark McKnight <mark.mcknight@yale.edu>
2018-08-07 13:26:32 -0400

    Implemented SyncSubStep in VerifyDownload.

--- src/components/sync/download/substeps/VerifyDownload.vue
+++ src/components/sync/download/substeps/VerifyDownload.vue
@@ -1,96 +1,91 @@
 <template>
-  <div>
-    <ul>
-      <li>
-        Verifying the download...
-        <strong v-if="success" class="green--text">OK.</strong>
-        <strong v-if="warning" class="amber--text">WARNING.</strong>
-        <strong v-if="error" class="red--text">ERROR.</strong>
-      </li>
-    </ul>
-    <span v-if="error" class="red--text">
-      <p>{{ errorMessage }}</p>
-    </span>
-    <span v-if="warning">
-      <p>{{ warningMessage }}</p>
-    </span>
-    <v-progress-linear
-      v-if="verifying"
-      height="2"
-      :indeterminate="true">
-    </v-progress-linear>
-    <v-btn
-      v-if="error || warning"
-      color="primary"
-      @click.native="retry">Retry</v-btn>
-    <v-btn
-      v-if="warning"
-      color="amber"
-      @click.native="ignore">Ignore</v-btn>
-  </div>
+  <sync-sub-step :working="verifying"
+                 :success="success"
+                 :current-log="currentLog"
+                 :ignore="ignore"
+                 :retry="retry">
+    Verifying the download...
+  </sync-sub-step>
 </template>
 
 <script>
     import config from '../../../../config'
     import FileService from '../../../../services/file/FileService'
+    import SyncSubStep from '../../SyncSubStep.vue'
+    import LoggingService, { defaultLoggingService } from '../../../../services/logging/LoggingService'
     export default {
       name: 'verify-download',
       data () {
         return {
           success: false,
-          warning: false,
-          error: false,
           verifying: false,
           apiRoot: config.apiRoot,
-          errorMessage: '',
-          warningMessage: ''
+          currentLog: undefined
         }
       },
       created () {
         this.verifyDownload()
       },
-      props: ['fileEntry', 'fileHash'],
+      props: {
+        fileHash: {
+          type: String,
+          required: true
+        },
+        fileEntry: {
+          type: Object,
+          required: true
+        },
+        loggingService: {
+          type: LoggingService,
+          required: false,
+          'default': function () { return defaultLoggingService }
+        }
+      },
       methods: {
         verifyDownload: function () {
           this.verifying = true
           FileService.calculateMD5Hash(this.fileEntry)
             .then((md5Hash) => {
-              console.log('serverProvidedHash', this.fileHash)
-              console.log('calculatedMd5Hash', md5Hash)
               if (md5Hash === this.fileHash) {
                 this.verifying = false
-                this.success = true
                 this.verificationDone()
               } else {
                 this.verifying = false
-                this.warning = true
-                this.warningMessage = 'Calculated hash does not match hash reported by the server.'
+                this.loggingService.log({
+                  severity: 'warn',
+                  message: 'Calculated hash does not match hash reported by the server.',
+                  serverProvidedHash: this.fileHash,
+                  calculatedMd5Hash: md5Hash
+                }).then((result) => { this.currentLog = result })
               }
-            },
-            (error) => {
-              this.extracting = false
-              this.error = true
-              console.error(error)
-              this.errorMessage = error.data
+            })
+            .catch((err) => {
+              this.verifying = false
+              this.loggingService.log(err).then((result) => { this.currentLog = result })
             })
         },
         verificationDone: function () {
+          this.success = true
           this.$emit('verify-download-done')
         },
         ignore: function () {
-          this.warning = false
-          this.success = true
-          this.verificationDone()
+          this.loggingService.log({
+            severity: 'info',
+            message: 'Warning ignored by user.'
+          }).then((log) => {
+            this.currentLog = log
+            this.verificationDone()
+          })
         },
         retry: function () {
-          this.error = false
-          this.warning = false
+          this.currentLog = undefined
           this.verifyDownload()
         }
       },
       computed: {
       },
       components: {
+        SyncSubStep
       }
     }
 </script>
