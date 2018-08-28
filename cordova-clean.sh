9de944ec95c7d4b3327f8fa5e5f812e637db2076
wyattis <wyattisrael@gmail.com>
2018-08-28 09:44:55 -0400

    Added cordova-clean.sh script to help mitigate these issues in the future. Also reduced the number of webpack builds required to start the mobile dev-server to only one.

---
+++ cordova-clean.sh
@@ -0,0 +1,28 @@
+cordova plugin rm cordova-plugin-device
+cordova plugin rm cordova-plugin-camera
+cordova plugin rm cordova-plugin-file
+cordova plugin rm cordova-plugin-file-md5
+cordova plugin rm cordova-plugin-zip
+cordova plugin rm cordova-plugin-splashscreen
+cordova plugin rm cordova-plugin-whitelist
+cordova plugin rm cordova-sqlite-storage
+cordova plugin rm cordova-plugin-file-transfer
+cordova plugin rm cordova-plugin-geolocation
+
+cordova platform rm android
+cordova platform rm ios
+
+cordova clean 
+
+cordova platform add android
+
+cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-camera
+cordova plugin add cordova-plugin-file
+cordova plugin add cordova-plugin-file-md5
+cordova plugin add cordova-plugin-zip
+cordova plugin add cordova-plugin-splashscreen
+cordova plugin add cordova-plugin-whitelist
+cordova plugin add cordova-sqlite-storage
+cordova plugin add cordova-plugin-file-transfer
+cordova plugin add cordova-plugin-geolocation
