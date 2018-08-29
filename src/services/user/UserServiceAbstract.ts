48f6f1d75e623c537ba3e24ac96fa1f5cd308a2f
Mark McKnight <mark.mcknight@yale.edu>
2018-08-29 10:28:40 -0400

    No longer storing user in local storage.

--- src/services/user/UserServiceAbstract.ts
+++ src/services/user/UserServiceAbstract.ts
@@ -1,28 +1,15 @@
-import storage from '../storage/StorageService'
 import User from '../../entities/trellis/User'
 
-const CURRENT_USER_KEY = 'current-user'
 export abstract class UserServiceAbstract {
 
   private _user: User
 
   set user (user: User) {
     this._user = user
-    storage.set(CURRENT_USER_KEY, user.toSnakeJSON())
   }
 
   get user (): User {
-    if (this._user) {
-      return this._user
-    } else {
-      let jsonUser = storage.get(CURRENT_USER_KEY)
-      let user = null
-      if (jsonUser) {
-        user = new User().fromSnakeJSON(jsonUser)
-        this._user = user
-      }
-      return user
-    }
+    return this._user
   }
 
   /**
@@ -52,8 +39,5 @@ export abstract class UserServiceAbstract {
    */
   removeCurrentUser (): void {
     delete(this._user)
-    storage.delete(CURRENT_USER_KEY)
   }
-
-
 }
