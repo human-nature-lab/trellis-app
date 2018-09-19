2be3b744df01f9869318e9bd85a5766cba7fce33
wyattis <wyattisrael@gmail.com>
2018-09-19 16:13:14 -0400

    Fixed up skip service tests and made the skip service easier to reason about without double negatives everywhere

--- test/services/skip-service.test.ts
+++ test/services/skip-service.test.ts
@@ -2,83 +2,124 @@ import SkipService from '../../src/components/interview/services/SkipService'
 import {expect} from 'chai'
 import Skip from "../../src/entities/trellis/Skip";
 
+function anyAllString (val: boolean): string {
+  return val ? 'all' : 'any'
+}
+
+function showHideString (val: boolean): string {
+  return val ? 'show' : 'hide'
+}
+
+function shouldHide (skips: Skip[], conditions: Set<string>): void {
+  expect(SkipService.shouldSkipPage(skips, conditions)).to.equal(true, 'This page should hide')
+}
+
+function shouldShow (skips: Skip[], conditions: Set<string>): void {
+  expect(SkipService.shouldSkipPage(skips, conditions)).to.equal(false, 'This page should show')
+}
+
+interface SkipHelper extends Skip {
+  show (): this
+  hide (): this
+  any (): this
+  all (): this
+}
+
+function makeSkip (conditions: string[]): SkipHelper {
+  let skip = new Skip().fromSnakeJSON({
+    show_hide: true,
+    any_all: true,
+    conditions: conditions.map(c => ({
+      id: c,
+      condition_tag_name: c
+    }))
+  })
+
+  // @ts-ignore
+  skip.show = function () {
+    skip.showHide = true
+    return skip
+  }
+  // @ts-ignore
+  skip.hide = function () {
+    skip.showHide = false
+    return skip
+  }
+  // @ts-ignore
+  skip.any = function () {
+    skip.anyAll = false
+    return skip
+  }
+  // @ts-ignore
+  skip.all = function () {
+    skip.anyAll = true
+    return skip
+  }
+
+  return skip as SkipHelper
+}
+
 export default function () {
   describe('SkipService', () => {
-    it('should handle show vs hide with no conditions', () => {
-      let skipCondition = new Skip().fromSnakeJSON({
-        show_hide: true,
-        any_all: true,
-        conditions: []
+    describe('show', () => {
+      describe('any', () => {
+        it('should hide if no conditions are present', () => {
+          shouldHide([makeSkip(['one']).show().any()], new Set([]))
+        })
+        it('should show if any condition is present', () => {
+          shouldShow([makeSkip(['one']).show().any()], new Set(['one']))
+          shouldShow([makeSkip(['one', 'two']).show().any()], new Set(['one']))
+          shouldShow([makeSkip(['one', 'two']).show().any()], new Set(['two']))
+        })
+        it('should hide if no conditions are present', () => {
+          shouldHide([makeSkip(['one']).show().any()], new Set(['two']))
+        })
       })
-      let conditionTags = new Set()
-      for (let anyAll of [true, false]) {
-        skipCondition.anyAll = anyAll
-        skipCondition.showHide = true
-        expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(true, `A "show" condition should skip if no condition tags are present. any_all: ${anyAll}`)
-        skipCondition.showHide = false
-        expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(false, `A "hide" condition should not skip if no conditions tags are present. any_all: ${anyAll}`)
-      }
-    })
-    it('should handle show vs hide with one condition tag', () => {
-      let skipCondition = new Skip().fromSnakeJSON({
-        show_hide: true,
-        any_all: true,
-        conditions: [{
-          id: 'wasda',
-          name: 'wasda'
-        }]
+      describe('all', () => {
+        it('should hide if no conditions are present', () => {
+          shouldHide([makeSkip(['one']).show().all()], new Set([]))
+        })
+        it('should show if all conditions are present', () => {
+          shouldShow([makeSkip(['one', 'two']).show().all()], new Set(['one', 'two']))
+          shouldShow([makeSkip(['one', 'two']).show().all()], new Set(['one', 'two', 'three']))
+        })
+        it('should hide if all conditions are not present', () => {
+          shouldHide([makeSkip(['one', 'two']).show().all()], new Set(['one']))
+          shouldHide([makeSkip(['one', 'two']).show().all()], new Set(['two']))
+          shouldHide([makeSkip(['one', 'two']).show().all()], new Set(['three']))
+        })
       })
-      let conditionTags = new Set(['wasda'])
-      for (let anyAll of [true, false]) {
-        skipCondition.anyAll = anyAll
-        skipCondition.showHide = true
-        expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(false, `A "show" condition for a single condition tag should never be skipped if that condition exists`)
-        skipCondition.showHide = false
-        expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(true, `A "hide" condition for a single condition tag should always be hidden in that condition exists`)
-      }
     })
-    it('should handle all vs any with show and hide', () => {
-      let skipCondition = new Skip().fromSnakeJSON({
-        show_hide: true,
-        any_all: true,
-        conditions: [{
-          id: 'wasda',
-          name: 'wasda'
-        }, {
-          id: 'hasda',
-          name: 'hasda'
-        }]
+    describe('hide', () => {
+      describe('any', () => {
+        it('should show if no conditions are present', () => {
+          shouldShow([makeSkip(['one']).hide().any()], new Set([]))
+        })
+        it('should hide if any conditions are present', () => {
+          shouldHide([makeSkip(['one']).hide().any()], new Set(['one']))
+          shouldHide([makeSkip(['one', 'two']).hide().any()], new Set(['one']))
+          shouldHide([makeSkip(['one', 'two']).hide().any()], new Set(['two']))
+        })
+        it('should show if no conditions are present', () => {
+          shouldShow([makeSkip(['one']).hide().any()], new Set(['two']))
+          shouldShow([makeSkip(['one', 'two']).hide().any()], new Set(['three', 'four']))
+        })
       })
-      let conditionTags = new Set(['wasda', 'hasda', 'canca'])
-      expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(false, `A "show if any" condition should be shown if any conditions match`)
-      skipCondition.showHide = false
-      expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(true, `A "hide if any" condition should be hidden if any conditions match`)
-      conditionTags = new Set(['wasda', 'canca'])
-      skipCondition.showHide = true
-      // Show if any conditions exist
-      expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(false, `A "show if any" condition should be shown if any conditions match`)
-      skipCondition.showHide = false
-      // hide if any conditions exist
-      expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(true, `A "hide if any" condition should be hidden if any conditions match`)
-      skipCondition.showHide = true
-      skipCondition.anyAll = false
-      // show if all conditions exist
-      expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(true, `A "show if all" condition should be hidden if all of the conditions don't match`)
-      skipCondition.showHide = false
-      // Hide if all conditions exist
-      expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(false, `A "hide if all" condition should be shown if all conditions don't match`)
-      conditionTags = new Set(['wasda', 'hasda'])
-      skipCondition.showHide = true
-      skipCondition.anyAll = false
-      expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(false, `A "show if all" condition should be shown if all conditions match`)
-      skipCondition.showHide = false
-      expect(SkipService.shouldSkipPage([skipCondition], conditionTags)).to.equal(true, `A "hide if all" condition should be hidden if all conditions match`)
-    })
-    it('should exit on the first show condition', () => {
-      // let skipConditions = [{
-      //   any_all: true,
-      //   show_hide: false
-      // }]
+      describe('all', () => {
+        it('should show if no conditions are present', () => {
+          shouldShow([makeSkip(['one']).hide().all()], new Set([]))
+        })
+        it('should hide if all conditions are present', () => {
+          shouldHide([makeSkip(['one']).hide().all()], new Set(['one']))
+          shouldHide([makeSkip(['one', 'two']).hide().all()], new Set(['one', 'two']))
+          shouldHide([makeSkip(['one', 'two', 'three']).hide().all()], new Set(['one', 'two', 'three', 'four', 'five']))
+        })
+        it('should show if all conditions are not present', () => {
+          shouldShow([makeSkip(['one']).hide().all()], new Set(['two']))
+          shouldShow([makeSkip(['one', 'two']).hide().all()], new Set(['two', 'three']))
+          shouldShow([makeSkip(['one', 'two']).hide().all()], new Set(['one', 'three']))
+        })
+      })
     })
   })
 }
