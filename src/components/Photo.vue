03e3275547ec295224e15ceaa8b0b67cd6adba1b
wyattis <wyattisrael@gmail.com>
2018-09-20 15:46:04 -0400

    Fixed placeholder image and added error event handling on failed image load

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -17,7 +17,7 @@
 
   // TODO: consider replacing this with a material design icon.
   // This can't be dynamically sized, so image is better
-  const URL_PLACEHOLDER = '../static/img/Placeholder_person.jpg'
+  const URL_PLACEHOLDER = require('../assets/Placeholder_person.jpg')
 
   const observer = new IntersectionObserver(handleIntersections, {
     threshold: 0.5
@@ -98,6 +98,10 @@
           let img = this.$refs.img
           if (!img) return
           img.addEventListener('load', function () {
+            _this.imgLoading = false
+            _this.imgLoaded = true
+          })
+          img.addEventListener('error', function () {
             _this.imgLoading = false
             _this.imgLoaded = true
           })
