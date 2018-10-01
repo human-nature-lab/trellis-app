2d520f9f68ccbf0d97223296f4cdeddea32532a5
Mark McKnight <mark.mcknight@yale.edu>
2018-10-01 14:04:47 -0400

    Changed max zoom level to 21.

--- src/components/geo/GeoSearchWithMap.vue
+++ src/components/geo/GeoSearchWithMap.vue
@@ -175,7 +175,7 @@
         })
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
           attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
+          maxZoom: 21,
           id: 'mapbox.streets',
           accessToken: '***REMOVED***'
         }).addTo(this.trellisMap)
