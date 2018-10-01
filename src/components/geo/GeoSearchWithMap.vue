89d656cad2f73e7a8093d1d787893d0044cf0591
Mark McKnight <mark.mcknight@yale.edu>
2018-10-01 12:39:48 -0400

    Increased maz zoom level from 18 to 21 on GeoSearchWithMap component.

--- src/components/geo/GeoSearchWithMap.vue
+++ src/components/geo/GeoSearchWithMap.vue
@@ -175,7 +175,7 @@
         })
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
           attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
+          maxZoom: 21,
           id: 'mapbox.streets',
           accessToken: 'pk.eyJ1IjoiZGlzcGVyc2UiLCJhIjoiSUtqRFhKZyJ9.eixdwJkaNkCiPPWew9i4pQ'
         }).addTo(this.trellisMap)
