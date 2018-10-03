905803cb48a554a9dec8ee57b498bee09b432769
Mark McKnight <mark.mcknight@yale.edu>
2018-10-03 12:42:11 -0400

    No longer setting deletedAt equal to null in TimestampedSoftDelete class.

--- src/entities/base/TimestampedSoftDelete.ts
+++ src/entities/base/TimestampedSoftDelete.ts
@@ -5,8 +5,8 @@ import MomentTransformer from "./MomentTransformer";
 import {Moment} from "moment";
 
 export default abstract class TimestampedSoftDelete extends Timestamped {
-  @Column({ name: "deleted_at", type: "datetime", nullable: true,transformer: MomentTransformer })
+  @Column({ name: "deleted_at", type: "datetime", nullable: true, transformer: MomentTransformer })
   @AsDate
   @Serializable
-  public deletedAt: Moment = null;
+  public deletedAt: Moment;
 }
