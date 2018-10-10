4b3d3680eff64485c7e86a55a66b1afdd7c9a3cc
Mark McKnight <mark.mcknight@yale.edu>
2018-10-10 15:07:54 -0400

    Removed a bunch of console.log statements. Now explicitly inserting Datum and QuestionDatum on add instead of using save method. Fixed InterpolationService with regards to fills inside of follow-up sections.

--- src/services/roster/RosterServiceWeb.ts
+++ src/services/roster/RosterServiceWeb.ts
@@ -2,12 +2,12 @@ import http from '../http/AxiosInstance'
 import RosterServiceInterface from './RosterServiceInterface'
 import Roster from '../../entities/trellis/Roster'
 
-
 export default class RosterServiceWeb implements RosterServiceInterface {
   async getRosterRows (rosterIds: string[]): Promise<Roster[]> {
     let res = await http().get(`rosters/${rosterIds.join(',')}`)
     return res.data.rosters.map(r => new Roster().fromSnakeJSON(r))
   }
+
   async createRosterRows (rosterRows: string[]): Promise<Roster[]> {
     let res = await http().post(`rosters`, {
       rosters: rosterRows
