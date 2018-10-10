dd798e4d87e219ebecc9c4d39bf7a1b94fd1dbe4
wyattis <wyattisrael@gmail.com>
2018-10-10 15:45:19 -0400

    Removed dropdown from AddPhoto component

--- src/components/photo/AddPhoto.vue
+++ src/components/photo/AddPhoto.vue
@@ -1,22 +1,26 @@
 <template>
-  <v-menu offset-y>
-    <v-btn slot="activator" icon>
-      <v-progress-circular v-if="isAddingPhoto" indeterminate />
-      <v-icon v-else>add</v-icon>
-    </v-btn>
-    <v-list>
-      <v-list-tile @click="photoFromCamera">
-        <v-list-tile-action>
-          <v-icon>photo_camera</v-icon>
-        </v-list-tile-action>
-      </v-list-tile>
-      <v-list-tile  @click="photoFromFile">
-        <v-list-tile-action>
-          <v-icon>cloud_upload</v-icon>
-        </v-list-tile-action>
-      </v-list-tile>
-    </v-list>
-  </v-menu>
+  <!--<v-menu offset-y>-->
+    <!--<v-btn slot="activator" icon>-->
+      <!--<v-progress-circular v-if="isAddingPhoto" indeterminate />-->
+      <!--<v-icon v-else>add</v-icon>-->
+    <!--</v-btn>-->
+    <!--<v-list>-->
+      <!--<v-list-tile @click="photoFromCamera">-->
+        <!--<v-list-tile-action>-->
+          <!--<v-icon>photo_camera</v-icon>-->
+        <!--</v-list-tile-action>-->
+      <!--</v-list-tile>-->
+      <!--<v-list-tile @click="photoFromFile">-->
+        <!--<v-list-tile-action>-->
+          <!--<v-icon>cloud_upload</v-icon>-->
+        <!--</v-list-tile-action>-->
+      <!--</v-list-tile>-->
+    <!--</v-list>-->
+  <!--</v-menu>-->
+  <v-btn @click="photoFromCamera" icon>
+    <v-progress-circular v-if="isAddingPhoto" indeterminate />
+    <v-icon v-else>add</v-icon>
+  </v-btn>
 </template>
 
 <script>
@@ -43,6 +47,7 @@
       async photoFromFile () {
         this.isAddingPhoto = true
         // TODO: Allow uploading photos from file
+        alert("File upload not supported yet")
         this.isAddingPhoto = false
       },
     }
