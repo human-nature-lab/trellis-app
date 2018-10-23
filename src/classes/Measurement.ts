6512f6556373485b13f5f2abf963f5306f35c8fa
wyattis <wyattisrael@gmail.com>
2018-10-23 15:22:27 -0400

    passing all the tests with optimized replaying strategy

--- src/classes/Measurement.ts
+++ src/classes/Measurement.ts
@@ -1,9 +1,19 @@
+interface Tick {
+  startTime: number
+  endTime: number
+  duration: number
+  min: number
+  max: number
+  average: number
+  name: string
+}
+
 export default class Measurement {
-  private min: number = Number.POSITIVE_INFINITY
-  private max: number = Number.NEGATIVE_INFINITY
-  private sum: number = 0
-  private n: number = 0
-  private startTime: number
+  public min: number = Number.POSITIVE_INFINITY
+  public max: number = Number.NEGATIVE_INFINITY
+  public sum: number = 0
+  public n: number = 0
+  public startTime: number
 
   constructor (public name: string) {}
 
@@ -19,11 +29,12 @@ export default class Measurement {
     this.startTime = performance.now()
   }
 
-  public stopTick () {
+  public stopTick (): Tick {
     if (!this.startTime) {
       throw new Error('Must start a tick before stopping one')
     }
-    const val = performance.now() - this.startTime
+    const endTime = performance.now()
+    const val = endTime - this.startTime
     if (val < this.min) {
       this.min = val
     }
@@ -33,6 +44,15 @@ export default class Measurement {
     this.startTime = null
     this.sum += val
     this.n++
+    return {
+      startTime: this.startTime,
+      endTime,
+      duration: val,
+      min: this.min,
+      max: this.max,
+      average: this.sum / this.n,
+      name: this.name
+    }
   }
 
   public get average (): number {
