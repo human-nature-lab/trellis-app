fb2392ede9cda9279f04365e27cdd6ad07f52e0c
Mark McKnight <mark.mcknight@yale.edu>
2018-10-25 16:34:16 -0400

    Fixed issue where the wrong census form (unpublished / deleted) may be loaded

--- src/services/census/CensusServiceCordova.ts
+++ src/services/census/CensusServiceCordova.ts
@@ -1,27 +1,24 @@
 import DatabaseService from '../database/DatabaseService'
 import CensusServiceAbstract from './CensusServiceAbstract'
 import StudyForm from '../../entities/trellis/StudyForm'
-import {IsNull, QueryBuilder} from "typeorm";
 import Form from "../../entities/trellis/Form";
 import FormService from "../form/FormService";
 
 export default class CensusServiceCordova extends CensusServiceAbstract {
 
   async getCensusForm (studyId: string, censusTypeId: string): Promise<Form> {
-    const repo = await DatabaseService.getRepository(StudyForm)
-    const studyForm: StudyForm = await repo.findOneOrFail({
-      where: {
-        deletedAt: IsNull(),
-        studyId: studyId,
-        censusTypeId: censusTypeId
-      }
-    })
+    const studyForm: StudyForm = await this.getCensusStudyForm(studyId, censusTypeId)
     return FormService.getForm(studyForm.formMasterId)
   }
 
   async hasCensusForm (studyId: string, censusTypeId: string): Promise<boolean> {
+    const studyForm: StudyForm = await this.getCensusStudyForm(studyId, censusTypeId)
+    return (studyForm instanceof StudyForm)
+  }
+
+  async getCensusStudyForm (studyId: string, censusTypeId: string): Promise<StudyForm> {
     const repo = await DatabaseService.getRepository(StudyForm)
-    const studyForm: StudyForm = await repo.createQueryBuilder('sf')
+    return repo.createQueryBuilder('sf')
       .where('sf.deletedAt is NULL')
       .andWhere('sf.studyId = :studyId', {studyId})
       .andWhere('sf.censusTypeId = :censusTypeId', {censusTypeId})
@@ -33,7 +30,6 @@ export default class CensusServiceCordova extends CensusServiceAbstract {
           .andWhere('form.deletedAt is NULL')
           .getQuery()
       ).getOne()
-    return (studyForm instanceof StudyForm)
   }
 
 }
