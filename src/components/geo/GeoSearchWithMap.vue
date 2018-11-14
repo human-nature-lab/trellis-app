7aaeae8968c489efccdd1d0e64344087d133a992
Mark McKnight <mark.mcknight@yale.edu>
2018-11-14 15:03:45 -0500

    Changed maxZoom level to 20 to match maximum rendered tiles.

--- src/components/geo/GeoSearchWithMap.vue
+++ src/components/geo/GeoSearchWithMap.vue
@@ -168,7 +168,7 @@
         // TODO: consider making the tile layer and accessToken configurable
         L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
           attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 21,
+          maxZoom: 20,
           id: 'mapbox.emerald',
           accessToken: 'pk.eyJ1IjoiZGlzcGVyc2UiLCJhIjoiSUtqRFhKZyJ9.eixdwJkaNkCiPPWew9i4pQ',
           style: 'mapbox://styles/mapbox/streets-v10'
