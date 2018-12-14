1bc9c6ff6f3cd07f5dde98219282a7fbb4227252
wyattis <wyattisrael@gmail.com>
2018-12-14 17:46:19 -0500

    Added documentation sidebar and linking stuff

---
+++ src/components/documentation/DocsLinkBadge.vue
@@ -0,0 +1,40 @@
+<template>
+  <span>
+    <v-badge
+         color="white"
+         right
+         v-if="isDebug">
+      <v-icon
+        dark
+        small
+        color="primary"
+        slot="badge"
+        @click="openDocs">help_outline</v-icon>
+      <slot />
+    </v-badge>
+    <slot v-else/>
+  </span>
+
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import bus, {DocsEventTypes} from './DocsEventBus'
+  export default Vue.extend({
+    name: 'DocsLinkBadge',
+    props: {
+      to: {
+        type: String
+      }
+    },
+    methods: {
+      openDocs () {
+        bus.$emit(DocsEventTypes.open, this.to)
+      }
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
