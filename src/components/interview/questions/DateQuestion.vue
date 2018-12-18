e17a7dc9eca1845e2564cc042563cbc4f37e83c5
Mark McKnight <mark.mcknight@yale.edu>
2018-12-18 11:46:55 -0500

    Added locale parameter to v-date-picker component.

--- src/components/interview/questions/DateQuestion.vue
+++ src/components/interview/questions/DateQuestion.vue
@@ -3,15 +3,27 @@
       v-model="date"
       reactive
       landscape
-      :disabled="isQuestionDisabled"/>
+      :locale="locale"
+      :disabled="isQuestionDisabled">
+    </v-date-picker>
 </template>
 
 <script>
   import QuestionDisabledMixin from '../mixins/QuestionDisabledMixin'
   import ActionMixin from '../mixins/ActionMixin'
   import AT from '../../../static/action.types'
+  import global from '../../../static/singleton'
+
   export default {
     name: 'date-question',
+    created () {
+      this.locale = global.locale.languageTag
+    },
+    data () {
+      return {
+        locale: 'en-us'
+      }
+    },
     mixins: [QuestionDisabledMixin, ActionMixin],
     props: {
       question: {
