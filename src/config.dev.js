5823430d4eca8645d83cb55311d75450fcd72bd9
wyattis <wyattisrael@gmail.com>
2019-01-04 16:37:03 -0500

    Added changelog and bumped version to 3.0.23

--- src/config.dev.js
+++ src/config.dev.js
@@ -3,10 +3,17 @@ import constants from './static/constants'
 const config = {
   appEnv: (typeof cordova === 'object') ? constants.APP_ENV.CORDOVA : constants.APP_ENV.WEB,
   appMode: constants.APP_MODE.PROD,
+  debug: true,
   xKey: '***REMOVED***',
-  apiRoot: 'https://api.***REMOVED***',
+  // apiRoot: 'http://10.0.1.6:8888',
+  apiRoot: 'http://api.trellisandroid.net',
+  // apiRoot: 'http://192.168.10.10',
+  // user: {
+  //   username: 'admin',
+  //   password: '***REMOVED***'
+  // },
   database: {
-    logging: ['warning', 'error']
+    logging: true
   }
 }
 
