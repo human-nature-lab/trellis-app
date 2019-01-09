988c36855af127e5ea31dc0677ca41dda649704e
wyattis <wyattisrael@gmail.com>
2019-01-09 16:05:30 -0500

    Version 3.0.25. Preload actions get the randomSortOrder and sortOrder assigned when they are copied over as actions.

--- src/config.prod.js
+++ src/config.prod.js
@@ -3,17 +3,10 @@ import constants from './static/constants'
 const config = {
   appEnv: (typeof cordova === 'object') ? constants.APP_ENV.CORDOVA : constants.APP_ENV.WEB,
   appMode: constants.APP_MODE.PROD,
-  debug: true,
   xKey: '***REMOVED***',
-  // apiRoot: 'http://10.0.1.6:8888',
-  apiRoot: 'http://api.trellisandroid.net',
-  // apiRoot: 'http://192.168.10.10',
-  // user: {
-  //   username: 'admin',
-  //   password: '***REMOVED***'
-  // },
+  apiRoot: 'https://api.***REMOVED***',
   database: {
-    logging: true
+    logging: ['warning', 'error']
   }
 }
 
