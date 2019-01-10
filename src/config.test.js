8dee764957772f6d99555d38d652c2b44a16c3c7
wyattis <wyattisrael@gmail.com>
2019-01-10 16:27:06 -0500

    Added speed measurement to webpack builds

--- src/config.test.js
+++ src/config.test.js
@@ -4,7 +4,7 @@ const config = {
   appEnv: (typeof cordova === 'object') ? constants.APP_ENV.CORDOVA : constants.APP_ENV.WEB,
   appMode: constants.APP_MODE.PROD,
   xKey: 'rXghvr7C1Q8dRmhX2Lyl3wC62TyoAr95',
-  apiRoot: 'https://api.trellisprod.net',
+  apiRoot: 'https://api.trellistest.net',
   database: {
     logging: ['warning', 'error']
   },
