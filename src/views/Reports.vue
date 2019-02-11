ba7fc35ad5f1a08f71a7cfda3615ef568ea23f2a
wyattis <wyattisrael@gmail.com>
2019-02-11 15:08:27 -0500

    Created a custom replacement for the v-circular-progress component which uses significantly less CPU. Reporting view now supports polling and asynchronous job handling.

--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -2,10 +2,13 @@
   <v-flex>
     <v-card>
       <v-container>
+        <v-progress-linear
+          v-if="isDownloading"
+          :value="downloadProgress" />
         <v-layout>
           <v-spacer />
           <v-btn
-            :disabled="!hasSelectedReports || isBusy"
+            :disabled="!hasSelectedReports || isDispatching"
             @click="dispatchReports">
             <v-progress-circular
               :size="20"
@@ -16,10 +19,6 @@
           <v-btn
             :disabled="isDownloading"
             @click="downloadReports">
-            <v-progress-circular
-              :size="20"
-              v-if="isDownloading"
-              indeterminate />
             {{$t('download_latest')}} ({{reportsToDownload.length}})
           </v-btn>
         </v-layout>
@@ -60,6 +59,12 @@
     report?: Report
   }
 
+  enum ReportStatus {
+    saved = 'saved',
+    queued = 'queued',
+    failed = 'failed'
+  }
+
   export default Vue.extend({
     name: 'Reports',
     mixins: [ DocsLinkMixin('reports/Introduction') ],
@@ -71,11 +76,14 @@
       return {
         global,
         isLoading: false,
-        isBusy: false,
+        isDispatching: false,
         isDownloading: false,
+        downloadProgress: 0,
         selectedStudyTypes: [],
         selectedForms: [],
-        reports: []
+        reports: [],
+        pollingRate: 10 * 1000,
+        isPolling: false
       }
     },
     created () {
@@ -96,60 +104,114 @@
         }
       },
       async dispatchReports (): Promise<void> {
-        this.isBusy = true
+        this.isDispatching = true
         try {
           const reports = await ReportService.dispatchReports(this.global.study.id, this.reportTypes, this.formIds)
-          this.updateReports(reports)
+          this.mergeReportsByType(reports)
+          this.startPolling()
+          this.clearSelected()
         } catch (err) {
           err.component = 'Reports.vue@dispatchReports'
           this.log(err)
           this.alert('error', this.$t('error'))
         } finally {
-          this.isBusy = false
+          this.isDispatching = false
         }
       },
-      updateReports (reports: Report[]) {
+      mergeReports (reports: Report[], comparator?: (r: Report, r2: Report) => boolean) {
+        if (!comparator) {
+          comparator = (r, r2) => r.id === r2.id
+        }
         for (let i = 0; i < reports.length; i++) {
           const report = reports[i]
-          const oldReport = this.reports.find(r => r.id === report.id)
-          if (oldReport) {
-            this.reports.splice(i, 1, oldReport)
+          const oldReportIndex = this.reports.findIndex(r => comparator(r, report))
+          if (oldReportIndex > -1) {
+            this.reports.splice(oldReportIndex, 1, report)
           } else {
             this.reports.push(report)
           }
         }
-        this.startPolling()
+      },
+      mergeReportsByType (reports: Report[]) {
+        return this.mergeReports(reports, (r: Report, r2: Report) => r.type + r.formId === r2.type + r2.formId)
+      },
+      clearSelected () {
+        this.selectedStudyTypes = []
+        this.selectedForms = []
       },
       startPolling (): void {
         // TODO: Poll for the status of queued reports
-        for (let report of this.reports) {
-          if (report.status === 'queued') {
-            // this.isBusy = true
+        const poll = async () => {
+          if (this.queuedReports.length) {
+            this.isPolling = true
+            try {
+              const reports = await ReportService.getReports(this.global.study.id, this.queuedReports.map(r => r.id))
+              this.mergeReports(reports)
+              if (this.queuedReports.length) {
+                setTimeout(poll, this.pollingRate)
+              } else {
+                this.isPolling = false
+              }
+            } catch (err) {
+              this.isPolling = false
+            }
+          } else {
+            this.isPolling = false
           }
+        }
+        if (!this.isPolling) {
+          poll()
         }
       },
       async downloadReports () {
         this.isDownloading = true
+        this.downloadProgress = 1
         const reportIds = this.reportsToDownload.map(r => r.id)
+        let prevProgress = 0
+        let n = 0
+        let stepPrediction = 20
         try {
-          const blob = await ReportService.getReportsZip(this.global.study.id, reportIds)
-          saveAs(blob, 'reports.zip')
+          const blob = await ReportService.getReportsZip(this.global.study.id, reportIds, (progress: ProgressEvent) => {
+            let totalEstimate
+            if (progress.lengthComputable) {
+              this.downloadProgress = 100 * progress.loaded / progress.total
+            } else {
+              // Here we just try to estimate the current progress using an integrator calibrated for about 20 steps.
+              // Nothing fancy, but necessary to do something like this without knowing the total.
+              n++
+              totalEstimate = progress.loaded + (stepPrediction / n) * ((progress.loaded - prevProgress) / 2)
+              this.downloadProgress = 100 * (progress.loaded / totalEstimate)
+              prevProgress = progress.loaded
+            }
+            console.log('progress', this.downloadProgress, progress.loaded, totalEstimate)
+          })
+          setTimeout(() => {
+            saveAs(blob, 'reports.zip')
+            this.isDownloading = false
+          }, 1000)
         } catch (err) {
           err.component = 'Reports.vue@downloadReports'
           this.log(err)
           this.alert('error', 'Unable to download reports')
-        } finally {
           this.isDownloading = false
+        } finally {
+          this.clearSelected()
         }
       }
     },
     computed: {
+      isBusy (): boolean {
+        return this.isPolling || this.isDispatching || this.isDownloading
+      },
       reportsToDownload (): Report[] {
         if (this.hasSelectedReports) {
-          return this.selected.filter(o => !!o.report && !!o.report.id && o.report.status === 'saved').map(o => o.report)
+          return this.selected.filter(o => !!o.report && !!o.report.id && o.report.status === ReportStatus.saved).map(o => o.report)
         } else {
           return this.reports
         }
+      },
+      queuedReports (): Report[] {
+        return this.reports.filter(r => r.status === ReportStatus.queued)
       },
       selected (): HasReport[] {
         return this.selectedForms.concat(this.selectedStudyTypes)
