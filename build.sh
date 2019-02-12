90f95a0f09ad013a781a1908a3a6de41ecdfd897
wyattis <wyattisrael@gmail.com>
2019-02-12 10:00:50 -0500

    version 3.0.27

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 TOKEN="***REMOVED***"
-node --experimental-modules build.js --config=src/config.prod.app.js --apk --skip-sentry --sentry-token=${TOKEN} --sentry-org="wyatt-israel"
+node --experimental-modules build.js --config=src/config.prod.app.js --apk --sentry-token=${TOKEN} --sentry-org="wyatt-israel"
 #node --experimental-modules build.js --config=src/config.prod.web.js --web --sentry-token=${TOKEN} --sentry-org="wyatt-israel"
