0a6c369e266608e49bfcee9f632686df512d9d4d
wyattis <wyattisrael@gmail.com>
2019-03-11 15:03:50 -0400

    Fixed configuration for trellistest

--- src/config.test.web.js
+++ src/config.test.web.js
@@ -1,17 +1,17 @@
 const constants = require('./static/constants')
 
 module.exports = {
-  appEnv: (typeof cordova === 'object') ? constants.APP_ENV.CORDOVA : constants.APP_ENV.WEB,
+  appEnv: constants.APP_ENV.WEB,
   appMode: constants.APP_MODE.PROD,
-  debug: true,
   xKey: '***REMOVED***',
-  apiRoot: 'http://api.trellisandroid.net',
-  formBuilderUrl: 'http://trellisandroid.net/#/form/{form_id}/builder?token={token}&study={study}&locale={locale}',
-  // apiRoot: 'http://10.0.1.222',
-  // apiRoot: 'http://10.0.1.6:8888',
-  // apiRoot: 'http://10.0.0.193:8888',
-  // apiRoot: 'http://192.168.10.10',
-  // apiRoot: 'https://api.***REMOVED***',
+  apiRoot: 'https://api.***REMOVED***',
+  debug: false,
+  database: {
+    logging: ['warning', 'error']
+  },
+  logging: {
+    levels: ['warn', 'error']
+  },
   mapTileLayer: {
     url: 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}',
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -20,21 +20,9 @@ module.exports = {
     accessToken: '***REMOVED***',
     style: 'mapbox://styles/mapbox/streets-v10'
   },
-  user: {
-    username: 'admin',
-    password: '***REMOVED***'
-  },
-  logging: {
-    levels: ['info', 'debug']
-  },
-  // database: {
-  //   //   logging: true
-  //   // },
   sentry: {
     dsn: '***REMOVED***',
-    onlineIntervalRate: 1 * 60 * 1000,    // frequency to check if we can send offline logs
+    onlineIntervalRate: 2 * 60 * 1000,    // frequency to check if we can send offline logs
     offline: true                         // True if we're using the offline integration
   }
 }
-
-console.log('ENV: ', module.exports.appEnv)
