01d6a00e6a4a22bbbb7bc7d4a938155857b5684a
wyattis <wyattisrael@gmail.com>
2019-03-21 11:53:24 -0400

    Added useful tags to Sentry logging messages. Closes #238.

--- src/logging.ts
+++ src/logging.ts
@@ -1,10 +1,32 @@
 import * as Sentry from '@sentry/browser'
+import Locale from "./entities/trellis/Locale";
 import Vue from 'vue'
 import config from './config'
+import Study from "./entities/trellis/Study";
+import DeviceService from "./services/device/DeviceService";
 import SentryOfflineTransport from "./services/logging/SentryOfflineTransport";
 import {BrowserOptions} from "@sentry/browser/esm";
+import StudyService from "./services/study/StudyService";
 import {APP_ENV} from './static/constants'
+import SingletonService from './services/SingletonService'
+import DatabaseService from './services/database/DatabaseService'
 declare const VERSION: string
+
+async function asyncSetup () {
+  let server = config.apiRoot
+  let deviceId = null
+  let snapshotId = null
+  if (config.appEnv === APP_ENV.CORDOVA) {
+    server = await DatabaseService.getServerIPAddress()
+    deviceId = await DeviceService.getUUID()
+    snapshotId = (await DatabaseService.getLatestDownload()).snapshotId
+  }
+  Sentry.configureScope(scope => {
+    scope.setTag('server', server)
+    scope.setTag('device', deviceId)
+    scope.setTag('snapshot', snapshotId)
+  })
+}
 
 if (config.sentry && config.sentry.dsn) {
   let release = 'trellis-' + VERSION
@@ -18,7 +40,6 @@ if (config.sentry && config.sentry.dsn) {
     integrations: [new Sentry.Integrations.Vue({ Vue }), new Sentry.Integrations.RewriteFrames()],
     debug: config.debug
   }
-  console.info('Using sentry for logging')
   if (config.sentry.offline) {
     console.info('SentryOfflineTransport implemented')
     sentryConfig.transportOptions = {
@@ -29,7 +50,35 @@ if (config.sentry && config.sentry.dsn) {
     }
     sentryConfig.transport = SentryOfflineTransport
   }
+  console.info('Using sentry for logging', sentryConfig)
   Sentry.init(sentryConfig)
+
+  // Subscribe to changes to the user and locale via the SingletonService
+  SingletonService.on('user', user => {
+    Sentry.configureScope(scope => {
+      if (user && user.id) {
+        scope.setUser({
+          id: user.id,
+          username: user.username
+        })
+      } else {
+        scope.setUser(null)
+      }
+    })
+  })
+  SingletonService.on('locale', (locale: Locale) => {
+    Sentry.configureScope(scope => {
+      scope.setTag('locale', locale.languageName)
+    })
+  })
+  SingletonService.on('study', (study: Study) => {
+    Sentry.configureScope(scope => {
+      scope.setTag('study', study.id)
+    })
+  })
+
+  asyncSetup()
+
 } else {
   console.info('Not using sentry for logging')
 }
