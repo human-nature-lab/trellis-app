bed611662157c607d041f4e3db8074905a72672b
wyattis <wyattisrael@gmail.com>
2019-04-05 16:42:06 -0400

    Version 3.0.30

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 TOKEN="***REMOVED***"
-#node build.js --config=src/config.prod.app.js --apk --sentry-token=${TOKEN} --sentry-org="human-nature-lab"
-node build.js --config=src/config.test.web.js --web --sentry-token=${TOKEN} --sentry-org="human-nature-lab"
+node build.js --config=src/config.prod.app.js --apk --sentry-token=${TOKEN} --sentry-org="human-nature-lab"
+#node build.js --config=src/config.test.web.js --web --sentry-token=${TOKEN} --sentry-org="human-nature-lab"
