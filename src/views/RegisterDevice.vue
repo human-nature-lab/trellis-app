e2325896dd04335220c22d794b846bfc33d7db65
wyattis <wyattisrael@gmail.com>
2019-04-17 12:00:06 -0400

    Added view for registering a device

---
+++ src/views/RegisterDevice.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-flex>
+    <v-form>
+      <v-text-field></v-text-field>
+    </v-form>
+  </v-flex>
+</template>
+
+<script>
+  export default {
+    name: 'RegisterDevice'
+  }
+</script>
+
+<style scoped>
+
+</style>
