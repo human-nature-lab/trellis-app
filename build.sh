3a212c5e4063e3cb09efe4c42482cfb8aa9ee368
wyattis <wyattisrael@gmail.com>
2019-06-05 16:12:11 -0400

    Removed sentry key from build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-TOKEN="***REMOVED***"
-./build.js --release --apk --sentry-token=${TOKEN} --skip-release
+./build.js --release --apk --sentry-token=${SENTRY_TOKEN} --skip-release
 #node build.js --config=src/config.test.web.js --web --sentry-token=${TOKEN} --sentry-org="human-nature-lab"
