4c836494528457bc437dc6bf61f4ffb85a6f69b1
Olga Maniati <olgmaniati@gmail.com>
2021-01-06 16:02:59 +0200

    Adding a button that would set updated_records.uploaded_at to null
    Working on adding a button that would set updated_records.uploaded_at to null

---
+++ src/components/import/ResetRecords.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-flex class="btn primary" >
+      <div class="btn__content" 
+      @click="setResetRecords('reset-records')">
+        {{$t('Clear Records')}}
+      </div>
+  </v-flex>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+  name: 'ResetRecords',
+  components: {},
+  data() {
+    return {
+      selectedResetRecords: 'reset-records'
+    };
+  },
+  methods: {
+    setResetRecords(tab) {
+      // TO DO: set updated_records.uploaded_at to null.
+      this.selectedResetRecords = tab;
+      this.alert('success', 'Reset records successfully')
+
+    }
+  }
+})
+</script>
+
+
+<style scoped>
+
+</style>
