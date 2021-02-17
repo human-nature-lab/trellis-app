a504cfc985a208ead45fa677ff054eacb5da2814
Mark McKnight <mark.mcknight@yale.edu>
2021-02-17 17:28:24 -0500

    Added GeoTypeSelect component to replace GeoTypeSelector.

---
+++ src/components/geo/GeoTypeSelect.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-select
+    outlined
+    dense
+    hide-details
+    :disabled="disabled"
+    :readonly="readonly || (clickToEdit && !editing)"
+    :append-icon="(clickToEdit && !editing) ? 'mdi-pencil' : ''"
+    @click:append="editing = true"
+    return-object
+    :items="geoTypes"
+    v-model="curGeoType"
+    item-text="name"
+    @change="selectGeoType"
+    :label="$t('geo_type')">
+  </v-select>
+</template>
+
+<script>
+  import GeoType from '../../entities/trellis/GeoType'
+  import GeoService from '../../services/geo/GeoService'
+  import global from '../../static/singleton'
+
+  export default {
+    name: 'GeoTypeSelect',
+    async created () {
+      try {
+        this.geoTypes = await GeoService.getGeoTypesByStudy(global.study.id, this.showUserAddable)
+        if (this.geoType) {
+          this.curGeoType = this.geoTypes.find((gt) => gt.id === this.geoType.id)
+        }
+      } catch (err) {
+        if (this.isNotAuthError(err)) {
+          this.logError(err)
+        }
+      }
+    },
+    props: {
+      disabled: {
+        type: Boolean,
+        required: false,
+        'default': false
+      },
+      clickToEdit: {
+        type: Boolean,
+        required: false,
+        'default': false
+      },
+      readonly: {
+        type: Boolean,
+        required: false,
+        'default': false
+      },
+      geoType: {
+        type: GeoType,
+        required: false
+      },
+      showUserAddable: {
+        type: Boolean,
+        required: false,
+        'default': false
+      },
+    },
+    data: function () {
+      return {
+        geoTypes: [],
+        curGeoType: null,
+        editing: false
+      }
+    },
+    methods: {
+      selectGeoType: function () {
+        if (this.editing && this.clickToEdit) {
+          this.editing = false
+        }
+        this.$emit('geoTypeSelected', this.curGeoType)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
