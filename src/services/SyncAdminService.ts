86070518fc55c8e88d76eb0c59e9b6b7276a8b11
wyattis <wyattisrael@gmail.com>
2022-05-27 16:54:06 -0400

    add infinite timeouts for making snapshots and publishing forms

--- src/services/SyncAdminService.ts
+++ src/services/SyncAdminService.ts
@@ -1,29 +1,27 @@
 import { adminInst } from './http/AxiosInstance'
 
 class SyncAdminService {
-
   async listUploads () {
-    const res = await adminInst.get(`list-uploads`)
+    const res = await adminInst.get('list-uploads')
     return res.data.uploads
   }
 
   async listSnapshots () {
-    const res = await adminInst.get(`list-snapshots`)
+    const res = await adminInst.get('list-snapshots')
     return res.data.snapshots
   }
 
   async processUploads (uploads: string[]) {
-    return adminInst.post(`process-uploads`, {
-      uploads
+    return adminInst.post('process-uploads', {
+      uploads,
     })
   }
 
   async generateSnapshot () {
-    return adminInst.post(`generate-snapshot`, {
-      timeout: 0
+    return adminInst.post('generate-snapshot', null, {
+      timeout: 0,
     })
   }
-
 }
 
 export default new SyncAdminService()
--- src/services/form/FormServiceWeb.ts
+++ src/services/form/FormServiceWeb.ts
@@ -6,7 +6,6 @@ import { saveAs } from 'file-saver'
 import Form from '../../entities/trellis/Form'
 import { uriTemplate } from '../http/WebUtils'
 export class FormServiceWeb implements FormServiceInterface {
-
   getStudyForms (studyId: string): Promise<StudyForm[]> {
     return http().get(uriTemplate('study/{study}/forms/published', [studyId])).then(res => {
       if (res.data.forms) {
@@ -23,7 +22,7 @@ export class FormServiceWeb implements FormServiceInterface {
     return res.data.forms.map(f => new StudyForm().fromSnakeJSON(f))
   }
 
-  getForm (formId: string, bareBones: boolean = false): Promise<Form> {
+  getForm (formId: string, bareBones = false): Promise<Form> {
     return http().get(uriTemplate('form/{form}', [formId]))
       .then(res => {
         if (res.data.form) {
@@ -38,7 +37,7 @@ export class FormServiceWeb implements FormServiceInterface {
 
   async createForm (studyId: string, formType: formTypes): Promise<StudyForm> {
     const res = await adminInst.post(uriTemplate('study/{study}/form', [studyId]), {
-      form_type: formType
+      form_type: formType,
     })
     return new StudyForm().fromSnakeJSON(res.data.form)
   }
@@ -69,8 +68,8 @@ export class FormServiceWeb implements FormServiceInterface {
     formData.append('formType', formType)
     const res = await adminInst.post(uriTemplate('study/{studyId}/form/import', [studyId]), formData, {
       headers: {
-        'Content-Type': 'multipart/form-data'
-      }
+        'Content-Type': 'multipart/form-data',
+      },
     })
     return new Form().fromSnakeJSON(res.data.importedForm)
   }
@@ -83,24 +82,27 @@ export class FormServiceWeb implements FormServiceInterface {
     const res = await adminInst.patch(uriTemplate('study/{study}/forms/reorder', [studyId]), { study_forms: studyForms })
     return res.data.forms.map(f => new StudyForm().fromSnakeJSON(f))
   }
-  
+
   async publishForm (studyId: string, formId: string): Promise<object> {
-    const res = await adminInst.post(uriTemplate('study/{study}/form/{form}/publish', [studyId, formId]))
+    const res = await adminInst.post(
+      uriTemplate('study/{study}/form/{form}/publish', [studyId, formId]),
+      null,
+      { timeout: 0 },
+    )
     return res.data
   }
-  
+
   async getVersions (formId: string): Promise<Form[]> {
     const res = await adminInst.get(uriTemplate('form/{form}/versions', [formId]))
     const versions = res.data.versions.map(f => new Form().fromSnakeJSON(f))
     versions.sort((a, b) => b.version - a.version)
     return versions
   }
-  
+
   async revertVersion (formMasterId: string, formVersionId: string): Promise<StudyForm> {
-    const res = await adminInst.put(uriTemplate('form/{form}/revert/{version}',[formMasterId, formVersionId]))
+    const res = await adminInst.put(uriTemplate('form/{form}/revert/{version}', [formMasterId, formVersionId]))
     return new StudyForm().fromSnakeJSON(res.data)
   }
-
 }
 
 export default FormServiceWeb
