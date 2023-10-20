1af8b1681698c152b4e17ec41be862fbe4fd00d5
wyattis <wyattisrael@gmail.com>
2023-10-20 17:12:07 -0400

    try again

--- build/build-cordova-config.js
+++ build/build-cordova-config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const mkdirp = require('mkdirp')
 const handlebars = require('handlebars')
 
 const isProd = process.env.NODE_ENV === 'production'
@@ -11,4 +12,5 @@ const output = path.join(__dirname, '../www/config.xml')
 data.ALLOW_CLEARTEXT_TRAFFIC = data.DEV || !!process.env.ALLOW_CLEARTEXT_TRAFFIC
 const template = handlebars.compile(fs.readFileSync(entry, 'utf8'))
 
+mkdirp.sync(path.dirname(output))
 fs.writeFileSync(output, template(data), 'utf8')
