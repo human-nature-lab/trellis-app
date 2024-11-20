66f2678c361a87250dc8e2d1a16f56401248f8ae
wyattis <wyattisrael@gmail.com>
2024-11-20 12:06:25 -0500

    make the demo banner play more nicely with the app

---
+++ src/components/AppBar.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import global from '../static/singleton'
+import { routeQueue } from '@/router'
+import { secondaryDrawerIcon, secondaryDrawerOnClick } from '@/helpers/drawer.helper'
+import { isTestStudy } from '@/helpers/singleton.helper'
+import Banner from './Banner.vue'
+
+const props = defineProps<{
+  maintenanceMode: boolean;
+  serverMode: string;
+  showBanner: boolean;
+}>()
+
+function toStudySelector () {
+  routeQueue.pushAndReturnToCurrent({ name: 'StudySelector' })
+}
+
+function toLocaleSelector () {
+  routeQueue.pushAndReturnToCurrent({ name: 'LocaleSelector' })
+}
+
+</script>
+
+<template>
+  <v-app-bar
+    app
+    absolute
+    elevate-on-scroll
+    scroll-target="#trellis-main"
+  >
+    <v-app-bar-nav-icon
+      :disabled="maintenanceMode"
+      @click="global.menuDrawer.open = !global.menuDrawer.open"
+    />
+    <v-toolbar-title class="logo">
+      <router-link
+        :to="{name: 'Home'}"
+        class="deep-orange--text"
+      >
+        <img
+          src="../assets/trellis-logo.png?url"
+          alt="trellis"
+        >
+      </router-link>
+    </v-toolbar-title>
+    <v-toolbar-title
+      v-if="global.study"
+      class="study"
+    >
+      <v-tooltip right>
+        <template #activator="{ on, attrs }">
+          <v-btn
+            class="subheading"
+            v-on="on"
+            v-bind="attrs"
+            :color="isTestStudy ? 'error' : null"
+            text
+            @click="toStudySelector"
+          >
+            {{ global.study.name }}
+            <v-icon
+              class="ml-2"
+              v-if="isTestStudy"
+              color="error"
+            >
+              mdi-dev-to
+            </v-icon>
+          </v-btn>
+        </template>
+        <span>{{ $t('change_study') }}</span>
+      </v-tooltip>
+    </v-toolbar-title>
+    <v-spacer />
+    <v-tooltip left>
+      <template #activator="{ on, attrs }">
+        <v-btn
+          class="subheading"
+          icon
+          v-bind="attrs"
+          v-on="on"
+          @click="toLocaleSelector"
+        >
+          {{ global.locale ? global.locale.languageTag : '' }}
+        </v-btn>
+      </template>
+      <span>{{ $t('change_locale') }}</span>
+    </v-tooltip>
+    <v-tooltip
+      v-if="secondaryDrawerIcon"
+      left
+    >
+      <template #activator="{on, attrs}">
+        <v-icon
+          v-on="on"
+          v-bind="attrs"
+          @click.stop="secondaryDrawerOnClick"
+        >
+          {{ secondaryDrawerIcon }}
+        </v-icon>
+      </template>
+      <span>{{ $t('view_current_documentation') }}</span>
+    </v-tooltip>
+
+    <template
+      v-if="showBanner"
+      #extension
+    >
+      <Banner :server-mode="serverMode" />
+    </template>
+  </v-app-bar>
+</template>
