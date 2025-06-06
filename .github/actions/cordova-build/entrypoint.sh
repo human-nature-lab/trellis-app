c1e2c2bb92f387f2318bfcc3be536bc91a02826d
wyattis <wyattisrael@gmail.com>
2025-06-06 14:22:39 -0400

    try to write the complete workflow

--- .github/actions/cordova-build/entrypoint.sh
+++ .github/actions/cordova-build/entrypoint.sh
@@ -3,3 +3,19 @@ set -e
 
 echo "pwd: $(pwd)"
 echo "ls -la: $(ls -la)"
+echo "gradle: $(which gradle)"
+echo "gradle version: $(gradle --version)"
+
+chmod +x -R hooks/*.sh
+
+npm ci
+cp static/config.default.js static/config.js
+cp config/config-xml.dev.default.js config/config-xml.dev.js
+
+npm run build:web
+zip -r trellis-web.zip www/
+
+npm run setup:cordova
+sh cordova-setup.sh
+npm run webpack:release:android
+mv platforms/android/app/build/outputs/apk/debug/app-debug.apk trellis-debug.apk
